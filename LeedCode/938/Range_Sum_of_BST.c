e261f18420dc57d1d41b5cb6116504c9676c58a0
mashalink <maria8link@gmail.com>
2024-01-09 22:29:45 +0200

    first task from leedcode

---
+++ LeedCode/938/Range_Sum_of_BST.c
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+int check_the_number_in_the_range(int number, int low, int high){
+    if (number >= low && number <= high)
+        return (1);
+    return (0);
+}
+
+int next_step(struct TreeNode* left, struct TreeNode*  right, int low, int high){
+    int sum = 0;
+
+    if (right != NULL)
+    {
+        if (check_the_number_in_the_range(right->val, low, high)){
+            sum += right->val;
+        }
+        sum += next_step(right->left, right->right, low, high);
+    }
+    if (left != NULL)
+    {
+        if (check_the_number_in_the_range(left->val, low, high)){
+            sum += left->val;
+        }
+        sum += next_step(left->left, left->right, low, high);
+    }
+    return (sum);
+} 
+
+int rangeSumBST(struct TreeNode* root, int low, int high) {
+    int sum = 0;
+    int next_var = 1;
+
+    if (check_the_number_in_the_range(root->val, low, high))
+        sum += root->val;
+    sum += next_step(root->left, root->right, low, high);
+    return (sum);
+}
